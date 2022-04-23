e0c96ae643023b150f9c550429b78b7f1579a121
16 Bit Games <16bitvideogame@gmail.com>
2022-04-22 21:19:20 -0300

    Updated System Check and Functionality
    Due to Python limitations and rethinking things System Check ensure that the base files and configs are ready before YourFlix starts. Removed Idea of pre-mounting drive and DB Checks since they require 3rd party Python Modules for functionality.
    
    Adding these functions to the pre-start in YourFlix

--- YourFlix Setup/YourFlixMKIII_A1.15.D1.14_all/usr/share/yourflix/yf_SystemCheck.py
+++ YourFlix Setup/YourFlixMKIII_A1.15.D1.14_all/usr/share/yourflix/yf_SystemCheck.py
@@ -1,14 +1,8 @@
 #! /usr/bin/env python
 import sys
 from BackgroundJobs import yf_PackageManager as PM
-from BackgroundJobs import yf_StorageManager as SM
 from BackgroundJobs import yf_ConfigManager as CM
-
-#Verification script to ensure fs integrity and db integrity before starting yf
-#This is to solve the 'Mike Problem' of the fs delete itself or database becomes corrupted
-# while in use. This is to make management easier for si/me
 
 if __name__ == '__main__':
     CM.LoadConfig()
-    SM.CheckMountedDrives()
-    #PM.VerifyPackageInstalls()
+    PM.VerifyPackageInstalls()
