bf1ce7662acab0af148cbc24d801741138d181e5
16 Bit Games <16bitvideogame@gmail.com>
2022-04-25 21:38:34 -0300

    Added Storage Drive Manager and compared Drives in config to db drives

--- YourFlix Setup/YourFlixMKIII_A1.15.D1.14_all/usr/share/yourflix/BackgroundJobs/yf_StorageManager.py
+++ YourFlix Setup/YourFlixMKIII_A1.15.D1.14_all/usr/share/yourflix/BackgroundJobs/yf_StorageManager.py
@@ -5,50 +5,28 @@ currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
 
-from Resources import yf_LinuxDefaults as LinuxDefaults
+from Resources import yf_Defaults as Defaults
+from Resources import yf_DBDefaults as DBDefaults
 from Utilities import yf_Log as Log
-from Utilities import PlatformCheck as PC
+from Database import yf_DbHandler as DB
 from Resources import yf_Execptions as YException
-
-#The Storage Manager is here to set the specifics of mounting Yourflix storage devices
-#and tracking it.
-
-def MountDrive(DriveID, FS, PHYSLOC):
-    if(PC.GetPlatform() == "Linux"):
-        Log.Debug("Mount Storage at Location")
-    else:
-        Log.Debug("Incompatible Platform, please use YourFlix on Linux")
-
-def UnMountDrive(DriveId):
-    if(PC.GetPlatform() == "Linux"):
-        Log.Debug("Unmounting Storage at Location")
-    else:
-        Log.Debug("Incompatible Platform, please use YourFlix on Linux")
-
-def GetAllStorageDrives():
-    if(PC.GetPlatform() == "Linux"):
-        Log.Debug("Getting Storage Drive")
-    else:
-        Log.Debug("Incompatible Platform, please use YourFlix on Linux")
+from BackgroundJobs import yf_DatabaseManager as DBManager
 
 def CheckMountedDrives():
-    if(PC.GetPlatform() == "Linux"):
-        Log.Debug("Verifing Drives and Mount locations")
-        if(os.path.exists(LinuxDefaults.Phys_ConfigLoc)):
-            Log.Debug("Path Exists "+LinuxDefaults.Phys_DbLoc)
-        else:
-            raise YException.MissingFileError("YourFlix config failed to generate or was deleted")
-    else:
-        Log.Debug("Incompatible Platform, please use YourFlix on Linux")
+    Log.Debug("Checking the Mounted Drives")
+    drivesToMount = Defaults.MountedDrives
+    drivesToMount += DBManager.GetMountedDrives('/usr/share/yourflix/yourflix.db')
+    checkMounted = []
+    for drive in drivesToMount:
+        if(drive not in checkMounted):
+            Log.Debug(drive[0])
+            checkMounted.append(drive)
+    Log.Debug(checkMounted)
+    MountDrives(checkMounted)
 
-def RestoreMountedDrives():
-    if(PC.GetPlatform() == "Linux"):
-        Log.Debug("Restoring/Rebuilding FSTAB")
-    else:
-        Log.Debug("Incompatible Platform, please use YourFlix on Linux")
-
-def CheckDatabase():
-    Log.Debug("Check if DB exists and update/rebuild if it does")
+def MountDrives(LIST_OF_DRIVES):
+    Log.Debug("Mounting the Drives")
+    Defaults.MountedDrives = LIST_OF_DRIVES
 
 if __name__ == '__main__':
     CheckMountedDrives()
--- YourFlix Setup/YourFlixMKIII_A1.15.D1.14_all/usr/share/yourflix/YourFlix.py
+++ YourFlix Setup/YourFlixMKIII_A1.15.D1.14_all/usr/share/yourflix/YourFlix.py
@@ -2,8 +2,13 @@
 import sys
 import sqlite3
 
-from BackgroundJobs import yf_DatabaseManager
+from BackgroundJobs import yf_DatabaseManager as DM
+from BackgroundJobs import yf_ConfigManager as CM
+from BackgroundJobs import yf_StorageManager as SM
+from Utilities import yf_Log as Log
 
 if __name__ == '__main__':
     CM.LoadConfig()
-    PM.VerifyPackageInstalls()
+    DM.PrepareDatabase()
+    SM.CheckMountedDrives()
+    CM.UpdateConfig()
